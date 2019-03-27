e9c0ec789f2557e07a483868d705f4fc2451ccc5
Eric Daams <eric@164a.com>
2019-03-27 12:56:21 +0930

    Fixed up a bug preventing the plugin from being activated. Also tidied up the styling a little.

--- charitable-progress-circle.php
+++ charitable-progress-circle.php
@@ -1,80 +1,87 @@
 <?php
 /**
- * Plugin Name:         Charitable - Progress Circle
- * Plugin URI:
- * Description:         Add a progress circle to the campaign page for campaigns with a goal. This will replace the usual campaign summary block with a customized "card" style block for the campaign.
- * Version:             0.1
- * Author:              WP Charitable
- * Author URI:          https://www.wpcharitable.com/
+ * Plugin Name: Charitable - Progress Circle
+ * Plugin URI:  https://github.com/Charitable/charitable-progress-circle
+ * Description: Add a progress circle to the campaign page for campaigns with a goal. This will replace the usual campaign summary block with a customized "card" style block for the campaign.
+ * Version:     0.2
+ * Author:      WP Charitable
+ * Author URI:  https://www.wpcharitable.com/
  */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 
-function charitable_template_campaign_percentage_raised( $campaign ) {
-    if ( ! $campaign->has_goal() ) {
-        return false;
-    }
+if ( ! function_exists( 'charitable_template_campaign_percentage_raised' ) ) {
 
-    $percent   = $campaign->get_percent_donated_raw();
-    $formatted = number_format( $percent, 2, charitable_get_currency_helper()->get_decimal_separator(), '' );
+	function charitable_template_campaign_percentage_raised( $campaign ) {
+		if ( ! $campaign->has_goal() ) {
+			return false;
+		}
 
-    wp_enqueue_script( 'jquery-easypiechart', plugin_dir_url( __FILE__ ) . 'jquery.easypiechart.min.js', array( 'jquery-core' ), '2.1.7', false );
-    wp_add_inline_script( 'jquery-easypiechart', '(function($){$(".charitable-percent-raised-chart").easyPieChart({ size: 190 })})(jQuery)' );
-?>
+		$percent   = $campaign->get_percent_donated_raw();
+		$formatted = number_format( $percent, 2, charitable_get_currency_helper()->get_decimal_separator(), '' );
+
+		wp_enqueue_script( 'jquery-easypiechart', plugin_dir_url( __FILE__ ) . 'jquery.easypiechart.min.js', array( 'jquery-core' ), '2.1.7', false );
+		wp_add_inline_script( 'jquery-easypiechart', '(function($){$(".charitable-percent-raised-chart").easyPieChart({ size: 190 })})(jQuery)' );
+		?>
 <style>
 .campaign-summary {
-    box-sizing: content-box;
-    float: left;
-    max-width: 190px;
-    margin-right: 1em;
+	box-sizing: content-box;
+	float: left;
+	min-width: 190px;
+	margin-right: 1em;
+	width: 100%;
 }
 .campaign-raised.campaign-summary-item {
-    width: 100%;
+	position: relative;
+	width: 100%;
 }
 .charitable-percent-raised-chart {
-    position: relative;
-    float: left;
-    width: 190px;
-    height: 190px;
-    line-height: 190px;
-    font-size: 2em;    
-    font-weight: bolder;
-    text-align: center;
+	position: relative;
+	float: left;
+	width: 190px;
+	height: 190px;
+	line-height: 190px;
+	font-size: 2em;
+	font-weight: bolder;
+	text-align: center;
 }
 .charitable-percent-raised-chart canvas {
-    position: absolute;
-    top: 0;
-    left: 0;
+	position: absolute;
+	top: 0;
+	left: 0;
 }
 .campaign-raised .charitable-amount-raised {
-    float: left;
-    text-align: left;
-    padding: 20px 0 0;
+	position: absolute;
+	left: 210px; /* 190 + 20 */
+	top: 50%;
+	transform: translateY(-50%);
 }
 .campaign-donors.campaign-summary-item,
 .campaign-time-left.campaign-summary-item {
-    border: none;
-    font-size: 0.9em;
+	border: none;
+	font-size: 0.9em;
 }
 </style>
 <div class="campaign-raised campaign-summary-item">
-    <div class="charitable-percent-raised-chart" data-percent="<?php echo esc_attr( $percent ) ?>" data-scale-color="false"><span><?php echo $formatted ?></span>%</div>
-    <div class="charitable-amount-raised"><?php echo $campaign->get_donation_summary() ?></div>
+	<div class="charitable-percent-raised-chart" data-percent="<?php echo esc_attr( $percent ); ?>" data-scale-color="false"><span><?php echo $formatted; ?></span>%</div>
+	<div class="charitable-amount-raised"><?php echo $campaign->get_donation_summary(); ?></div>
 </div>
-<?php
+		<?php
 
-    return true;
+		return true;
+	}
 }
 
 function ed_unhook_default_template_functions() {
-    /** 
-     * This hook is defined on line 70 of charitable/includes/public/charitable-template-hooks.php
-     * 
-     * The original `add_action()` call looks like this: 
-     * add_action( 'charitable_campaign_summary', 'charitable_template_campaign_donor_count', 8 ); 
-     *
-     * To unhook it, we copy this line entirely and change 'add' to 'remove'.
-     */
-    remove_action( 'charitable_campaign_summary', 'charitable_template_campaign_donation_summary', 6 );
+	/**
+	 * This hook is defined on line 46 of charitable/includes/public/charitable-template-hooks.php
+	 *
+	 * The original `add_action()` call looks like this:
+	 * add_action( 'charitable_campaign_summary', 'charitable_template_campaign_donation_summary', 6 );
+	 *
+	 * To unhook it, we copy this line entirely and change 'add' to 'remove'.
+	 */
+	remove_action( 'charitable_campaign_summary', 'charitable_template_campaign_donation_summary', 6 );
 }
-add_action( 'after_setup_theme', 'ed_unhook_default_template_functions', 11 ); 
+
+add_action( 'after_setup_theme', 'ed_unhook_default_template_functions', 11 );
